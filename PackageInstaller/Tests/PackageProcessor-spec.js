42d489867bd70bac5442df9b455e9821e3d3ec0d
Tyler Rasor <tylerrasor@hotmail.com>
2017-06-30 16:48:02 -0600

    Create file structure and script and test files.  Create test to verify that I am able to load modules.

---
+++ PackageInstaller/Tests/PackageProcessor-spec.js
@@ -0,0 +1,11 @@
+﻿//PackageProcessor-spec.js
+
+'use strict';
+var expect = require('chai').expect;
+
+describe('PackageProcessor', function () {
+    it('Should Exist', function () {
+        var PackageProcessor = require('../Scripts/PackageProcessor.js');
+        expect(PackageProcessor).to.not.be.undefined;
+    });
+});
