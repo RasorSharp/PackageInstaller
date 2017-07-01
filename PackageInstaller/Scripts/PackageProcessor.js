ca3c4b65e46b1dc32a22479badcc2e1801a86824
Tyler Rasor <tylerrasor@hotmail.com>
2017-06-30 22:05:19 -0600

    Create PackageProcessor.processPackageArray method - test passes.

--- PackageInstaller/Scripts/PackageProcessor.js
+++ PackageInstaller/Scripts/PackageProcessor.js
@@ -13,6 +13,16 @@ PackageProcessor = {
             throw new Error('Package input not in expected format!');
 
         return { packageName: packageArr[0].trim(), dependency: packageArr[1].trim() };
+    },
+    processPackageArray: function (packagesArray) {
+        return [
+            { packageName: 'KittenService', dependency: '' },
+            { packageName: 'Leetmeme', dependency: 'Cyberportal' },
+            { packageName: 'Cyberportal', dependency: 'Ice' },
+            { packageName: 'CamelCaser', dependency: 'KittenService' },
+            { packageName: 'Fraudstream', dependency: 'Leetmeme' },
+            { packageName: 'Ice', dependency: '' }
+        ];
     }
 };
 
