b44301a35ab58e3da38eba0e976ef5a007e650b7
Tyler Rasor <tylerrasor@hotmail.com>
2017-06-30 21:16:52 -0600

    Added test cases for invalid inputs

--- PackageInstaller/Tests/PackageProcessor-spec.js
+++ PackageInstaller/Tests/PackageProcessor-spec.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 var PackageProcessor = require('../Scripts/PackageProcessor.js');
 
 describe('parsePackage', function () {
-    it('Takes a properly formatted string and returns an object with package name and dependency', function () {
+    it('Takes a properly formatted string and returns an object with package name and dependency.  Invalid inputs cause error.', function () {
         var input = 'KittenService: CamelCaser';
         var expectedOutput = { packageName: 'KittenService', dependency: 'CamelCaser' };
         var actual = PackageProcessor.parsePackage(input);
@@ -16,5 +16,20 @@ describe('parsePackage', function () {
         actual = PackageProcessor.parsePackage(input);
 
         expect(actual).to.eql(expectedOutput);
+
+        //tests for unexpected inputs
+        input = 'KittenService CamelCaser';
+        expect(PackageProcessor.parsePackage(input)).to.throw(Error);
+
+        input = 'KittenService';
+        expect(PackageProcessor.parsePackage(input)).to.throw(Error);
+
+        input = '';
+        expect(PackageProcessor.parsePackage(input)).to.throw(Error);
+
+        input = null;
+        expect(PackageProcessor.parsePackage(input)).to.throw(Error);
+        //end unexepected inputs
+
     });
 });
